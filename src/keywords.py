2118f3a6b0a0976cadc094f00192fdd98bc9e523
abencarrington <99282629+abencarrington@users.noreply.github.com>
2025-01-22 15:28:44 -0500

    Updated Files

---
+++ src/keywords.py
@@ -0,0 +1,19 @@
+# List of teams you want to track (exact or partial matches).
+TEAMS = [
+    "Liverpool"
+]
+
+# List of leagues you want to track.
+LEAGUES = [
+    "Premier League",
+]
+
+# List of cups or tournaments you want to track.
+CUPS = [
+    "FA Cup",
+    "UEFA Champions League"
+]
+
+# Desired timezone for your calendar events.
+# Must match a TZ identifier recognized by your environment (e.g., 'Europe/London', 'America/New_York')
+TIMEZONE = "America/New_York"
